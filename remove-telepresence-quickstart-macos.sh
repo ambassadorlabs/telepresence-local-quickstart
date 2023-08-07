c465610a3950ac0ea77fe70eff8166aa6f20fe9e
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2023-08-07 14:16:57 -0600

    update documentation in scripts
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- remove-telepresence-quickstart-macos.sh
+++ remove-telepresence-quickstart-macos.sh
@@ -4,5 +4,8 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail
 
+echo "Deleting kind-telepresence-quickstart cluster"
 kind delete cluster -n telepresence-quickstart
+
+echo "Removing kind binary installed at /usr/local/bin/kind"
 sudo rm -rf /usr/local/bin/kind
--- telepresence-quickstart-setup-macos.sh
+++ telepresence-quickstart-setup-macos.sh
@@ -4,12 +4,23 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail
 
-###### Check for prerequisites
+###### Check for prerequisites ######
+### Check to see if Docker is installed
 if ! command -v docker &> /dev/null
 then
     echo "Docker could not be found, it is a prerequisite for Kind, please install Docker for your system."
     exit
 fi
+
+### Check to see if Telepresence is installed
+if ! command -v telepresence &> /dev/null
+then
+  echo "Telepresence not installed, please install it from https://www.getambassador.io/docs/telepresence/latest/install and re-run this script."
+else
+  echo "Telepresence already installed, moving on!"
+fi
+
+######
 
 ###### Create a Kind cluster ######
 ### Install Kind only if they don't have Kind
@@ -38,15 +49,9 @@ fi
 ### Make sure the kind context is the current one
 kubectl config use-context kind-telepresence-quickstart
 
-### Check to see if telepresence is installed
-if ! command -v telepresence &> /dev/null
-then
-  echo "Telepresence not installed, please install it from https://www.getambassador.io/docs/telepresence/latest/install and re-run this script."
-else
-  echo "Telepresence already installed, moving on!"
-fi
+######
 
-### Install traffic manager
+###### Install traffic manager ######
 echo "Installing Telepresence traffic manager to cluster"
 if telepresence helm install ;
 then
@@ -55,7 +60,9 @@ else
   echo "Failed to install the Telepresence traffic manager."
 fi
 
-# Install edgey-corp-python
+######
+
+###### Install edgey-corp-python ######
 echo "Installing sample app"
 if kubectl apply -f ./edgey-corp-python/k8s-config/edgey-corp-web-app-no-mapping.yaml ;
 then
@@ -63,3 +70,5 @@ then
 else
   echo "Failed to install the sample app."
 fi
+
+######
